a15d2159049dcc65476c65ceb7ae224d46ff1070
Sergio-Garcia-Acuna <s.garcia.1@estudiantec.cr>
2026-08-18 22:17:01 -0600

    Tercer commit: para el inciso 6 se añadió una función sumar_variables creada para observar 3 errores identificables por flake8

---
+++ Tarea 1/sumar_variables_flake8_errores.py
@@ -0,0 +1,11 @@
+def sumar_variables(a, b):
+    print("Suma de variables: ", a + b)
+    return a + b
+
+
+""" ERRORES DADOS POR FLAKE 8"""
+"""
+sumar_variables.py:1:1: D100 Missing docstring in public module
+sumar_variables.py:1:1: D103 Missing docstring in public function
+sumar_variables.py:2:11: Q000 Double quotes found but single quotes preferred
+"""
