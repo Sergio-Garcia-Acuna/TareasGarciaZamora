93e106d4ff9474f7b933bab851cc7f3d559f5fa8
Sergio-Garcia-Acuna <s.garcia.1@estudiantec.cr>
2026-08-18 22:20:33 -0600

    Cuarto commit: para el inciso 7 se arreglaron los 3 errores detectados por flake8 en el archivo .py de la función sumar_variables

--- Tarea 1/sumar_variables_flake8_errores.py
+++ Tarea 1/sumar_variables_flake8_errores.py
@@ -1,11 +1,7 @@
+"""FUNCION SUMAR VARIABLES PARA DEMOSTRAR ERRORES ENCONTRADOS POR FLAKE8."""
+
+
 def sumar_variables(a, b):
-    print("Suma de variables: ", a + b)
+    """SUMA DE VARIABLES donde a y b son valores numéricos."""
+    print('Suma de variables: ', a + b)
     return a + b
-
-
-""" ERRORES DADOS POR FLAKE 8"""
-"""
-sumar_variables.py:1:1: D100 Missing docstring in public module
-sumar_variables.py:1:1: D103 Missing docstring in public function
-sumar_variables.py:2:11: Q000 Double quotes found but single quotes preferred
-"""
